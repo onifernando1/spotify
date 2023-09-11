80936f81b926d75176c79d3a281e198106f2e594
onifernando1 <onifernando1@gmail.com>
2023-09-12 00:26:29 +0100

    download mp3 from yt

--- server/app.js
+++ server/app.js
@@ -44,4 +44,27 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
+const videoId = "YLslsZuEaNE";
+console.log("hey");
+
+const getVideoInfo = async (id) => {
+  try {
+    const stream = await ytdl(id, { quality: "highestaudio" });
+    let start = Date.now();
+    ffmpeg(stream)
+      .audioBitrate(128)
+      .save(`/home/onifernando1/spotify/a.mp3`)
+      .on("progress", (p) => {
+        readline.cursorTo(process.stdout, 0);
+        process.stdout.write(`${p.targetSize}kb downloaded`);
+      })
+      .on("end", () => {
+        console.log(`\ndone, thanks - ${(Date.now() - start) / 1000}s`);
+      });
+  } catch (error) {
+    console.error(error);
+  }
+};
+getVideoInfo(videoId);
+
 module.exports = app;
